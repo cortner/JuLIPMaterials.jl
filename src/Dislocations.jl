e8d6684344843729fec0eeacc064ad3b17d14c77
Christoph Ortner <c.ortner@warwick.ac.uk>
2016-10-13 20:34:56 +0100

    First draft of fcc_edge stuff. still need to replace the isotropic solution with the anisotropic solution though.

--- src/Dislocations.jl
+++ src/Dislocations.jl
@@ -14,12 +14,106 @@ function check_fcc(S::AbstractString)
    @assert vecnorm(F/F[1,2] - Afcc) < 1e-12
 end
 
+const Abcc = JMatF([ -1.0 1 1; 1 -1 1; 1 1 -1])
+
+"""
+ensure that species S actually crystallises to BCC
+"""
+function check_bcc(S::AbstractString)
+   F = defm(bulk(S))
+   return vecnorm(F/F[1,2] - Abcc) < 1e-12
+end
 
 
+"""
+`fcc_edge_plane(s::AbstractString) -> at::ASEAtoms, b, xcore `
+
+Generates a unit cell for an FCC crystal with orthogonal cell vectors chosen
+such that the F1 direction is the burgers vector and the F3 direction the normal
+to the standard edge dislocation:
+   b = F1 ~ a1;  ν = F3 ~ a2-a3
+The third cell vector: F2 ~ a * e1. Here, ~ means they are rotated from the
+a1, a2, a3 directions. This cell contains two atoms.
+
+Returns
+* `at`: Atoms object
+* `b` : Burgers vector
+* `xcore` : a core-offset (to add to any lattice position)
+"""
+function fcc_edge_plane(s::AbstractString)
+   # ensure s is actually an FCC species
+   check_fcc(s)
+   # get the cubic unit cell dimension
+   a = ( bulk(s, cubic=true) |> defm )[1,1]
+   # construct the cell matrix
+   F = JMat( [a/√2   0    0;
+                 0   a    0;
+                 0   0    a/√2 ] )
+   X = [ JVec([0.0, 0, 0]),
+         JVec([a/√8, a/2, a/√8]) ]
+   # construct ASEAtoms
+   at = ASEAtoms(string(s,"2"), X)
+   set_defm!(at, F, updatepositions=false)
+   # compute a burgers vector in these coordinates
+   b = a/√2 * JVec([1.0,0.0,0.0])
+   # compute a core-offset (to add to any lattice position)
+   xcore = a/√2 * JVec([1/2, 1/3, 0])  # [1/2, 1/3, 0]
+   # return the information
+   return at, b, xcore, a
+end
 
 
+project12(x) = SVec{2}([x[1],x[2]])
 
 
+"""
+`fcc_edge_geom(s::AbstractString, R::Float64) -> at::ASEAtoms`
+
+generates a linear elasticity predictor configuration for
+an edge dislocation in FCC, with burgers vector ∝ e₁  and dislocation
+core direction ν ∝ e₃
+"""
+function fcc_edge_geom(s::AbstractString, R; truncate=true)
+   # compute the correct unit cell
+   atu, b, xcore, a = fcc_edge_plane(s)
+   # multiply the cell to fit a ball of radius a/√2 * R inside
+   L1 = ceil(Int, 2*R) + 3
+   L2 = ceil(Int, 2*R/√2) + 3
+   at = atu * (L1, L2, 1)
+   # mess with the data
+   # turn the Burgers vector into a scalar
+   @assert b[2] == b[3] == 0.0
+   b = b[1]
+   # compute a dislocation core
+   xcore = project12(xcore)
+   X12 = project12.(positions(at))
+   # compute x, y coordinates relative to the core
+   x, y = mat(X12)[1,:], mat(X12)[2,:]
+   xc, yc = mean(x), mean(y)
+   r² = (x-xc).^2 + (y-yc).^2
+   I0 = find( r² .== minimum(r²) )[1]
+   xcore = X12[I0] + xcore
+   x, y = x - xcore[1], y - xcore[2]
+   # compute the dislocation predictor
+   # >>>>>> TODO: need the "real" one; this is just for testing <<<<<<<
+   ux, uy = u_edge_isotropic(x, y, b, 0.25)
+   # apply the linear elasticity displacement
+   X = positions(at) |> mat
+   X[1,:], X[2,:] = x + ux + xcore[1], y + uy + xcore[2]
+   # if we want a circular cluster, then truncate to an approximate ball (disk)
+   if truncate
+      F = defm(at) # store F for later use
+      X = vecs(X)  # find points within radius
+      IR = find( [vecnorm(x[1:2] - xcore) for x in X] .<= R * a/√2 )
+      X = X[IR]
+      at = ASEAtoms("$s$(length(X))")  # generate a new atoms object
+      set_defm!(at, F)                 # and insert the old cell shape
+   end
+   # update positions in Atoms object, set correct BCs and return
+   set_positions!(at, X)
+   set_pbc!(at, (false, false, true))
+   return at, xcore
+end
 
 
 
@@ -35,85 +129,12 @@ not be the isotropic elasticity solution.
 """
 function u_edge_isotropic(x, y, b, ν)
    x[y .< 0] += b/2
-   rsq = x.^2 + y.^2
-   ux = b/(2*π) * ( arctan(x ./ y) + (x .* y) ./ (2*(1-ν) * rsq) )
-   uy = -b/(2*π) * ( (1-2*ν)/(4*(1-ν)) * log(rsq) + (x.^2 - y.^2) ./ (4*(1-nu) * rsq) )
+   r² = x.^2 + y.^2
+   ux = b/(2*π) * ( atan(x ./ y) + (x .* y) ./ (2*(1-ν) * r²) )
+   uy = -b/(2*π) * ( (1-2*ν)/(4*(1-ν)) * log(r²) + (y.^2 - x.^2) ./ (4*(1-ν) * r²) )
    return ux, uy
 end
 
 
-
-
-
-project12(x, E) = [dot(x, E[1]); dot(x, E[2])]
-
-
-function basis_a12(at)
-    F = defm(at)
-    a1, a2, a3 = F[:,1], F[:,2], F[:,3]
-    a1 /= vecnorm(a1); a2 /= vecnorm(a2); a3 /= vecnorm(a3)
-    e1 = a1
-    e2 = a2 - dot(a1, a2) * a1; e2 /= vecnorm(e2)
-    e3 = e1 × e2
-    return (e1, e2, e3)
-end
-
-
-function pos2d_fcc(at)
-    X = positions(at)
-    e1, e2, e3 = basis_a12(at)
-    x = [dot(e1, x) for x in X]
-    y = [dot(e2, x) for x in X]
-    z = [dot(e3, x) for x in X]
-    zmax = maximum(z) / 2
-    z = [ round(Int, zi/zmax) for zi in z ]
-    I0 = find(z .== 0)
-    I1 = find(z .== 1)
-    I2 = find(z .== 2)
-    return x, y, I0, I1, I2
-end
-
-
-
-
-"""
-generates a reference configuration
-"""
-function edge_fcc(S::AbstractString, R::Float64)
-   check_fcc(S)
-   # nearest-neighbour distance
-   a = rnn(S)
-   # elementary cell
-   at = bulk(S)
-   E = basis(at)
-
-   # calculate how often we need to repeat it
-   # ----------------------------------------
-   Fu = defm(at)
-   a1, a2, a3 = Fu[:,1], Fu[:,2], Fu[:,3]
-   # compute inner radius of the cell in the a1-a2 plane
-   a1perp = a1 - dot(a1, a2) / vecnorm(a2)^2
-   Ri = 0.5 * vecnorm(a1perp)
-   # this means we need to repeat the cell Ri/R times in a1, a2 directions
-   #   (add 3 for good measure)
-   # and 3 times in the a3 direction to get one each of the ABC planes
-   nrep = ceil(Int, R/Ri) + 3
-   at = at * (nrep, nrep, 3)
-
-   # compute an origin
-   # ------------------
-   F = defm(at)
-   X = positions(at)
-   # project into 2D
-   x, y, I0, I1, I2 = pos2d_fcc(at)
-   # find the site closest to the centre of the cell
-   rsq = (x - mean(x)).^2 + (y - mean(y)).^2
-   Ictr = find(rsq .== minimum(rsq))
-   xctr = X[Ictr]
-   # the origin is shifted from xctr into one of the cells
-
-   #
-   # ------------------
-end
 
 end
