63a833acaf922ac78ab43fe15a0b894fd3816515
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-06-14 23:13:33 +0100

    finished implementation of general dislocation solution (still need to test and debug it though)

--- src/bbs.jl
+++ src/bbs.jl
@@ -14,8 +14,8 @@ typealias Mat3{T} SMatrix{3,3,T}
 typealias Ten33{T} SArray{(3,3,3),T,3,27}
 typealias Ten43{T} SArray{(3,3,3,3),T,4,81}
 
-# extend `angle` to avoid going via C
-Base.angle(x, y) = Base.angle(x + im * y)   # atan2(y, x)
+# extend `angle` to avoid going via ℂ
+Base.angle(x, y) = atan2(y, x)
 
 "convert normalised vector to spherical coordinates"
 spherical(x) = angle(x[1], x[2]), angle(norm(x[1:2]), x[3])
@@ -23,7 +23,7 @@ spherical(x) = angle(x[1], x[2]), angle(norm(x[1:2]), x[3])
 "convert spherical to euclidean coordinates on the unit sphere"
 euclidean(φ, ψ) = Vec3(cos(ψ) * cos(φ), cos(ψ) * sin(φ), sin(ψ))
 
-"given a vector x ∈ R³, return `(z0, z1)` where `(x/norm(x),z0,z1)` form an ONB."
+"given a vector x ∈ ℝ³, return `z0, z1` where `(x/norm(x),z0,z1)` form an ONB."
 function onb{T}(x::Vec3{T})
    x /= norm(x)
    φ, ψ = spherical(x)
@@ -35,17 +35,32 @@ end
 inv3x3(A) = inv( Mat3(A) )
 
 # contract(a, C, b) -> D  where D_ij = C_injm a_n b_m
-contract(a::Vec3, C::Ten43, b::Vec3) =
-   Mat3([dot(C[i,:,j,:] * b, a) for i=1:3, j = 1:3])
+# contract(a::Vec3, C::Ten43, b::Vec3) = Mat3([dot(C[i,:,j,:] * b, a) for i=1:3, j = 1:3])
+# the following is a meta-programming trick (due to @keno) which basically makes
+# the compiler write out the entire expression without intermediate allocation
+#   (edit: this works only on v0.6; on v0.5 not much is gained)
+# @eval contract(a, C, b) = Mat3($(Expr(:tuple, (:(dot(C[$i,:,$j,:] * b, a)) for i=1:3, j=1:3)...)))
 
 contract2(C::Ten43, a::Vec3) = constract(a, C, a)
 
-
-quad(f, Nquad) = sum(f, range(0, 2*pi/Nquad, Nquad)) * 2*pi / Nquad
-z(z0, z1, ω) = cos(ω) * z0 + sin(ω) * z1
-
+⊗(a::Vec3, b::Vec3) = a * b'
 
 # ========== Implementation of the BBS79 formula for a dislocation =============
+
+function QSB(m0, n0, Nquad)
+   Q, S, B = zero(Mat3), zero(Mat3), zero(Mat3)
+   for ω in range(0, pi/Nquad, Nquad)
+      m = cos(ω) * m0 + sin(ω) * n0
+      n = sin(ω) * m0 + cos(ω) * n0
+      nn⁻¹ = inv3x3( contract2(C, n) )
+      nm = contract(n, C, m)
+      mm = contract2(C, m)
+      Q += nn⁻¹                           # (3.6.4)
+      S += nn⁻¹ * nm                      # (3.6.6)
+      B += mm - nm' * nn⁻¹ * nm           # (3.6.9) and using  mn = nm'  (potential bug?)
+   end
+   return Q * (-1/Nquad), S * (-1/Nquad), B * (1/4/Nquad/pi)
+end
 
 """
 u_bbs(x, b, ν, C) -> u
@@ -57,12 +72,21 @@ the dislocation line is assumed to pass through the origin
 * t : dislocation direction (tangent to dislocation line)
 * C : elastic moduli 3 x 3 x 3 x 3
 """
-function u_bbs(x, b, t, C, Nquad = 10)
+function grad_u_bbs(x, b, t, C, Nquad = 10)
    x, b, t, C = Vec3(x), Vec3(b), Vec3(t), Ten43(C)
    t /= norm(t)
-   m, n = onb(t)
-   Q = quad( ω -> inv3x3( contract2(C, z(ω)) ) * (-1/pi)
-   S = quad( ω ->
+   m0, n0 = onb(t)   # some refrence ONB for computing Q, S, B
+   Q, S, B = QSB(m0, n0, Nquad)
+   # compute displacement gradient
+   # project x to the (m0, n0) plane and compute the (m, n) vectors
+   x = x - dot(x, t) * t
+   r = norm(x)
+   m = x / norm(x)
+   n = m × t
+   #  Implement (4.1.16)
+   nn⁻¹ = inv3x3( contract(n, C, n) )
+   nm = contract(n, C, m)
+   return 1/(2*π*r) * ( (- S * b) ⊗ m + (nn⁻¹ * ((2*π*B + nm*S) * b)) ⊗ n )
 end
 
 
