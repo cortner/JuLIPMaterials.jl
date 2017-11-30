e9e7e3d44188f7839ff8cb2b0d7630c888f9489f
Christoph Ortner <c.ortner@warwick.ac.uk>
2017-11-30 13:28:47 +0000

    some aggressive pruning

--- src/MaterialsScienceTools.jl
+++ src/MaterialsScienceTools.jl
@@ -10,30 +10,11 @@ include("Si.jl")
 
 include("Testing.jl")
 
-
-
 # include("BCC.jl")
 
-#
-# include("Elasticity_110.jl")
-#
-# include("Dislocations.jl")
-#
-# include("Silicon.jl")
-#
-# include("Dislocations_Silicon_hard.jl")
-#
-# include("Dislocations_Silicon_hard_110.jl")
-#
-# include("Dislocations_Al_hard_110.jl")
-#
-# include("no_dislocation.jl")
-#
-#
 # include("TriangularLattice.jl")
-#
+
 # include("PointDefects.jl")
-#
-# include("stevecubic.jl")
+
 
 end # module
