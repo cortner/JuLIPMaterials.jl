05c43c53126300b98093bb83a03bc70fb7887fba
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-03-08 15:40:32 +0000

    cleanup tests, added example notebook

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Base.Test
 
 tests = [
    # "test_elasticity.jl",   # TODO: this one is a joke, needs to be redone
-   # "test_greensfunctions.jl",
-   # "test_dislocations.jl",
+   "test_greensfunctions.jl",
+   "test_dislocations.jl",
    "test_cauchyborn1.jl",
 ]
 
