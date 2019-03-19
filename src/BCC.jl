4d975830d3f93ea9a608f97ab729fe8d4b13434f
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-03-19 22:24:55 +0000

    bugfixes in bcc screw dislocation predictor

--- src/BCC.jl
+++ src/BCC.jl
@@ -45,7 +45,7 @@ Construct a circular cluster with a screw dislocation in the centre.
 """
 function screw_111(sym::Symbol, R::Float64;
             x0 = :center, layers=1, soln = :anisotropic, calc = nothing,
-            bsign = 1)::AbstractAtoms
+            bsign = 1, Nquad=30)::AbstractAtoms
    a0, b0, c0 = lattice_constants_111(sym; calc=calc)
    x00 = JVecF( ([b0, 0, a0/3] + [b0/2, c0, 2*a0/3]) / 3 )
    if (x0 == :center) || (x0 == :centre)
@@ -53,7 +53,8 @@ function screw_111(sym::Symbol, R::Float64;
       x0 = x00
    end
    # create a cluster
-   at = cluster(cell_111(sym, calc=calc), R, dims=[1,2], x0=x0)
+   atu = cell_111(sym, calc=calc)
+   at = cluster(atu, R, dims=[1,2], x0=x0)
    at = at * (1,1,layers)
    # get positions  to manipulate them
    X = positions(at) |> mat
@@ -73,7 +74,6 @@ function screw_111(sym::Symbol, R::Float64;
       X = vecs(X)
       X .+= disl.(X0)
    elseif soln == :anisotropic
-      atu = cell_111(sym)
       set_pbc!(atu, true)
       set_calculator!(atu, calc)
       t = Vec3([0.0, 0.0, 1.0])
@@ -81,8 +81,7 @@ function screw_111(sym::Symbol, R::Float64;
       m0 = Vec3([1.0, 0.0, 0.0])
       ℂ = CLE.elastic_moduli(atu)
       ℂ = Ten43{Float64}(ℂ[:])
-      @show typeof(ℂ)
-      disl = CLE.Dislocation(b, t, ℂ, Nquad = 20, cut = m0, remove_singularity = true)
+      disl = CLE.Dislocation(b, t, ℂ, Nquad = Nquad, cut = m0, remove_singularity = true)
       X0 = vecs(X0)
       X = vecs(X)
       X .+= disl.(X0)
